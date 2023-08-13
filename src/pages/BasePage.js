863c6338147b4ee48237610640f0b1ff65675315
xuanhieu <nguyenxuanhieuktb10@gmail.com>
2023-08-13 23:34:53 +0700

    add skeleton image when loading new image

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import PhotoList from "components/PhotoList";
+import SkeletonLoadingImage from "components/SkeletonLoadingImage";
 
 const BasePage = ({
     fnc,
@@ -16,7 +17,7 @@ const BasePage = ({
     const fetchData = () => {
         fnc(fncParam).then((data) => {
             setIsLoading(false);
-            if(isResults)
+            if (isResults)
                 setPhotos(data.results);
             else setPhotos(data);
         });
@@ -24,12 +25,19 @@ const BasePage = ({
 
     const fetchDataNext = () => {
         setIsLoading(true);
+        setPhotos((prev) => [...prev, SkeletonLoadingImage, SkeletonLoadingImage, SkeletonLoadingImage, SkeletonLoadingImage, SkeletonLoadingImage, SkeletonLoadingImage])
         fncNext(fncParam).then((data) => {
             setIsLoading(false);
-            if(isResults)
-                setPhotos((prev) => [...prev, ...data.results]);
-            else 
-                setPhotos((prev) => [...prev, ...data]);
+            if (isResults)
+                setPhotos((prev) => {
+                    const updatedPrev = prev.slice(0, prev.length - 6);
+                    return [...updatedPrev, ...data.results];
+                });
+            else
+                setPhotos((prev) => {
+                    const updatedPrev = prev.slice(0, prev.length - 6);
+                    return [...updatedPrev, ...data];
+                });
         });
     };
 
