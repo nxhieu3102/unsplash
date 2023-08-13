22d17a50d5cc0077198813f0290ac88a37c3d0c4
xuanhieu <nguyenxuanhieuktb10@gmail.com>
2023-08-13 23:34:16 +0700

    skeleton loading

---
+++ src/components/SkeletonLoading.js
@@ -0,0 +1,12 @@
+
+const SkeletonLoading = () => {
+    return (
+        <div class="container w-[100vw] h-[100vh]">
+            <div class="card h-full w-full">
+                <div class="card-image loading w-full h-full"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonLoading;
---
+++ src/components/SkeletonLoadingImage.js
@@ -0,0 +1,12 @@
+
+const SkeletonLoadingImage = () => {
+    return (
+        <div class="container w-full h-[500px]">
+            <div class="card h-full w-full">
+                <div class="card-image loading w-full h-full"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonLoadingImage;
