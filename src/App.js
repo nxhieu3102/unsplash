657b93a82348392e060371474536619ea7735ede
xuanhieu <nguyenxuanhieuktb10@gmail.com>
2023-08-13 16:59:21 +0700

    add route search

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "pages/Home";
 import Collection from "pages/Collection";
 import Layout from "components/Layout";
+import Search from "pages/Search";
 
 export default function App() {
 	const routes = [
 		{ path: "/", element: <Home /> },
-		{ path: "/collection/:name", element: <Collection /> }
+		{ path: "/collection/:name", element: <Collection /> },
+		{ path: "/search/q=:query", element: <Search />}
 	];
 
 	return (
