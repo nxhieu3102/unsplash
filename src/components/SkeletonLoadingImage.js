d20a618c565574555677f820a4edc90f860e5b1e
xuanhieu <nguyenxuanhieuktb10@gmail.com>
2023-08-14 12:18:59 +0700

    class -> className

--- src/components/SkeletonLoadingImage.js
+++ src/components/SkeletonLoadingImage.js
@@ -1,9 +1,9 @@
 
 const SkeletonLoadingImage = () => {
     return (
-        <div class="container w-full h-[500px]">
-            <div class="card h-full w-full">
-                <div class="card-image loading w-full h-full"></div>
+        <div className="container w-full h-[500px]">
+            <div className="card h-full w-full">
+                <div className="card-image loading w-full h-full"></div>
             </div>
         </div>
     )
