c5db82827208372c1e81d9fabf65f7c72932670d
xuanhieu <nguyenxuanhieuktb10@gmail.com>
2023-08-14 12:18:51 +0700

    modify gutter of item on a column

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -2,16 +2,32 @@ import React from "react";
 import Photo from "./Photo";
 import Masonry from "react-responsive-masonry";
 
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+
 const PhotoList = ({ list }) => {
+    const parentElement = getElementByXpath("/html/body/div[1]/div/div/div[2]/div/div");
+    if (parentElement) {
+        const childElements = parentElement.querySelectorAll('div');
+        if (childElements.length > 0) {
+            childElements.forEach(childElement => {
+                childElement.style.gap = "20px"; 
+            });
+        } 
+    } 
+
     return (
-        <Masonry columnsCount={3} gutter="20px">
+        <Masonry columnsCount={3} gutter="66px">
             {list.map((Item, index) => {
                 return (
-                    <div className="w-full" key={index}>
+                    <div className="w-[400px]" key={index}>
                         {
                             Item.urls ?
                                 <Photo
-                                    src={Item.urls.raw}
+                                    src={Item.urls.small}
                                     width={Item.width}
                                     height={Item.height}
                                     altDescription={Item.alt_description}
